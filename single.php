f93d44fdeddb7e16df0f4b7f26a690f2013acd50
Claudia De Leon <claulg2701@gmail.com>
2017-03-11 00:48:11 -0500

    styling post

--- single.php
+++ single.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-	<div class="row">
+	<div class="outline row">
 		<div class="twelve columns">
 <!-- Begin single PHP -->
 			<?php if (have_posts()) :
@@ -10,12 +10,54 @@
               <?php the_post_thumbnail('large'); ?>
             </div>
         <?php } ?>
-					<h1><?php the_title(); ?></h1>
-					<?php the_content();?>
-          <div class="post-tags"><?php the_tags("","","");?></div>
-				<?php endwhile;
-			endif; ?>
-<!-- End single PHP -->
+          <div class="post-padding">
+            <h1><?php the_title(); ?></h1>
+            <div id="post-info">
+              <?php
+              $author = get_the_author();;
+              echo substr($author, 0,10);
+              echo "\n\n";
+              the_time('M jS, Y');
+              echo "\n\n";
+              the_category(' ');?>
+              </div>
+              <?php the_content();?>
+              <div class="post-tags"><?php the_tags("","","");?></div>
+
+
+              <div id="cooler-nav" class="navigation">
+
+              <?php
+              $prevPost = get_previous_post(true);
+              $nextPost = get_next_post(true);
+
+              if($prevPost) {?>
+              <div class="nav-box previous">
+              <?php
+                $prevthumbnail = get_the_post_thumbnail($prevPost->ID, 'thumbnail', array(100,100));
+                $prevExcert = get_the_excerpt( $prevPost->ID );
+                previous_post_link('%link',"$prevthumbnail  <p>%title</p>", TRUE);
+              ?>
+              </div>
+              <?php }
+
+              if($nextPost) { ?>
+              <div class="nav-box next">
+              <?php
+                $nextthumbnail = get_the_post_thumbnail($nextPost->ID, 'thumbnail', array(100,100));
+                next_post_link('%link',"$nextthumbnail  <p>%title</p>", TRUE);
+              ?>
+              </div>
+              <?php } ?>
+
+              </div><!--#cooler-nav div -->
+
+
+
+  				   <?php endwhile;
+  			     endif; ?>
+          <!-- End single PHP -->
+          </div>
 		</div>
 	</div>
 <?php get_footer(); ?>
