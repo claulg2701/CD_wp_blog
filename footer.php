c6e8870cc86d4317cd4c3bfec8549f749a54876e
Claudia De Leon <claulg2701@gmail.com>
2017-03-11 22:32:30 -0500

    theme footer

--- footer.php
+++ footer.php
@@ -1,13 +1,20 @@
       <!-- BEGINNING OF FOOTER - END SECTION CONTENT -->
       <footer class="row">
-        <div class="four columns">
-          <?php dynamic_sidebar('footer-widget-one'); ?>
+        <div class="social-media twelve columns">
+          <ul>
+            <li class="sm facebook"><a href="https://www.facebook.com/claulg2701/"><i class="fa fa-facebook fa-lg"></i></a></li>
+
+      			<li class="sm instagram"><a href="http://www.instagram.com/claulg2701"><i class="fa fa-instagram fa-lg"></i></a></li>
+
+      			<li class="sm linkedin"><a href="https://www.linkedin.com/in/claulg2701/"><i class="fa fa-linkedin fa-lg"></i></a></li>
+
+      			<li class="sm twitter"><a href="https://twitter.com/claulg2701"><i class="fa fa-twitter fa-lg"></i></a></li>
+
+      			<li class="sm googleplus"><a href="https://plus.google.com/u/0/+ClaudiaDeLeonGutierrez"><i class="fa fa-google-plus fa-lg"></i></a></li>
+          </ul>
         </div>
-        <div class="four columns">
-          <?php dynamic_sidebar('footer-widget-two'); ?>
-        </div>
-        <div class="four columns">
-          <?php dynamic_sidebar('footer-widget-three'); ?>
+        <div class="twelve columns copyright">
+          <p>Coppyright 2017 &copy; | Claudia De Leon | Powered by WordPress.</p>
         </div>
       </footer>
     </div> <!--end container class-->
