a9021f5ceac4b6623913c7a0b791629fd7471010
Claudia De Leon <claulg2701@gmail.com>
2017-03-13 17:24:57 -0400

    style search page

--- search.php
+++ search.php
@@ -2,21 +2,37 @@
 /* Template Name: Search Page */
 get_header(); ?>
 
-<div class="row">
-	<div class="eight columns">
-			<h1>
-				<?php printf( __('Search Results for: %s'), '<span>' . get_search_query() . '</span>' );?>
+<div class="outline row">
+	<div class="home twelve columns">
+			<h1 class="related">
+				<?php printf( __('Search Results for: %s'), '<span style="color:#40b4bc;">' . get_search_query() . '</span>' );?>
 			</h1>
 
 			<?php if (have_posts()) {
-				      while (have_posts()) :
-                the_post(); ?>
-                <h2><?php the_title(); ?></h2>
-		            <?php the_content();
-				      endwhile;
+				      while (have_posts()) :?>
+                <div class="home-post">
+                  <?php
+                  the_post();
+                  if ( has_post_thumbnail() ) { ?>
+                      <a href="<?php the_permalink();?>" class = "post-img"><?php the_post_thumbnail('thumbnail'); } ?></a>
+                      <div class="post-summary">
+                        <a href="<?php the_permalink(); ?>">
+                          <h5 class="nextPrevTitle"><?php the_title(); ?></h5>
+                        </a>
+                        <div class="home post-info">
+                          <?php
+                          $author = get_the_author();
+                          echo substr($author, 0,10)." | \n\n";
+                          the_time('M jS, Y');?>
+                        </div><!--end home-post-info-->
+                        <p class="nextPrevEx"><?php echo wp_trim_words( get_the_content(), 25, ' ' ); ?>
+                        <a href="<?php the_permalink(); ?>">Read More</a></p>
+                      </div> <!--end home-summary-->
+                  </div> <!--end home-post-->
+				      <?php endwhile;
 		       } else { ?>
-        			<h1>Nothing Found</h1>
-        			<p>Sorry, but nothing matched your search criteria. Please try again with different search terms.</p>
+        			<h1 class="related">Nothing Found</h1>
+        			<p class="related">Sorry, but nothing matched your search criteria. Please try again with different search terms.</p>
 		<?php } ?>
 	</div>
 </div>
