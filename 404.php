4ed1a7ecbb2f6c57dfce6dfe5ff6f82f9118f6c8
Claudia De Leon <claulg2701@gmail.com>
2017-03-13 19:16:48 -0400

    project documentation

--- 404.php
+++ 404.php
@@ -3,17 +3,12 @@
 
 get_header(); ?>
 
-	<div class="row">
-		<div class="eight columns">
+	<div class="outline row">
+		<div class="related twelve columns">
 			<h1>Not Found</h1>
-			<h2>This is somewhat embarrassing, isn't it?</h2>
-			<p>It looks like nothing was found at this location. Maybe try a search?</p>
-
-			<h2>Try searching for what you need:</h2>
-			<?php get_search_form(); ?>
-		</div>
-		<div class="four columns">
-			<?php get_sidebar(); ?>
+			<h5>This is somewhat embarrassing, isn't it?</h5>
+			<p>It looks like nothing was found at this location.</p>
+      <h5>Try a Search instead.</h5>
 		</div>
 	</div>
 
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Claudia's Blog
 ###### by Claudia De Leon
+
+* Basic Portfolio Template for WordPress
+
+* Running at [www.claudia-deleon.com/mmc6145](http://www.claudia-deleon.com/mmc6145).
+
+##Details
+* Index or Home Page has a fixed layout with flexible widget bar on the "Contact Me" section.
+* Basic support for Pages
+* Complete support of Category pages
+* Complete support of post pages.
+
+### Required Plugins
+* WPForms Lite - for "Contact Me" section
+
+###Suggested Feature Image Dimensions
+* Size: (w x h) in px 2656 × 821
+* Resolution: 72
+* File Format: png
